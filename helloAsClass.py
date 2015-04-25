4c34667e74faa31e1e9d44c03c84d3945be16005
Krzysiek Nowakowski <n.krzysiek@gmail.com>
2015-04-25 22:26:22 +0200

    certificate downloaded working

--- helloAsClass.py
+++ helloAsClass.py
@@ -1,4 +1,6 @@
+import os
 from subprocess import call
+import subprocess
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import sys
@@ -11,14 +13,14 @@ class Form:
         win = QWidget()
         layout = QGridLayout(win)
 
+        self.button1 = QPushButton(win)
+        self.button1.setText("Install AGH certificate")
+        layout.addWidget(self.button1)
+
         self.passwordField = QLineEdit()
         self.passwordField.setObjectName("sudo_password")
         self.passwordField.setEchoMode(QLineEdit.Password)
         layout.addWidget(self.passwordField)
-
-        self.button1 = QPushButton(win)
-        self.button1.setText("Install AGH certificate")
-        layout.addWidget(self.button1)
 
         self.button2 = QPushButton(win)
         self.button2.setText("exit")
@@ -38,7 +40,34 @@ class Form:
 
     def button1Action(self):
         print(self.passwordField.text())
+        Certificate.downloadCertificate(Certificate, self.passwordField.text())
 
+
+
+
+class Certificate:
+    urlHandler = urllib.URLopener()
+    certificateUrl = "https://panel.agh.edu.pl/CA-AGH/CA-AGH.der"
+    certificateFile = "/usr/local/share/ca-certificates/CA-AGH.der"
+
+
+    @staticmethod
+    def downloadCertificate(self, sudo_password):
+        temp_file = "/tmp/etc_hosts.tmp"
+
+        self.urlHandler.retrieve(self.certificateUrl, temp_file)
+
+        command_mv = "echo " + str(sudo_password) + "| sudo -S -k mv " + temp_file + " " + self.certificateFile
+
+        # with open(os.devnull, 'wb') as devnull:
+        #     subprocess.check_call([command_mv], stdout=devnull, stderr=subprocess.STDOUT)
+        os.system(command_mv)
+
+
+    @staticmethod
+    def reloadCertificates(sudo_password):
+        command = "echo " + str(sudo_password) + "| sudo -S -k update-ca-certificates"
+        os.system(command)
 
 
 
